bfd8672844011ffc358abd208f5c7ee6dca02500
Taiwei Ko <152499488+macgeek39@users.noreply.github.com>
2024-01-01 23:22:52 -0600

    Finished Homework

--- HomeworkTypeCoversions/Program.cs
+++ HomeworkTypeCoversions/Program.cs
@@ -6,3 +6,15 @@
  * Print that information to the console in natural language.
  */
 
+// Identify how old is user
+Console.Write("How old are you: ");
+string? age = Console.ReadLine();
+
+// Add 25 years to the user
+int ageAdd = int.Parse(age) + 25;
+
+// Subtract 25 years to the user
+int ageSubtract = int.Parse(age) - 25;
+
+// Print all that info to the console
+Console.WriteLine($"You are currently {age} years old, you will be {ageAdd} years old in 25 years and you were {ageSubtract} years old 25 years ago!");
