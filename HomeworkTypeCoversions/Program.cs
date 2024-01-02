3d126706208174ed50bf5affd5b9be1eefea5696
Taiwei Ko <152499488+macgeek39@users.noreply.github.com>
2024-01-01 23:14:27 -0600

    Initialized commit

--- HomeworkTypeCoversions/Program.cs
+++ HomeworkTypeCoversions/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+/*
+ * Capture a user's age from the console and
+ * then identify how old they will be in 
+ * 25 years, as well as how old they were 25 years ago.
+ * Print that information to the console in natural language.
+ */
+
